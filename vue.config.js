c9e68e5cbab5627edaeeafb7ff6a70c637fd53e0
hamdi hamza <hamdihamzadev@hotmail.com>
2024-09-17 14:17:39 +0100

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'? '/dashbFront/': '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/': '/',
   devServer: {
-    historyApiFallback: true  // This ensures the app serves index.html for any route
+    historyApiFallback: true  
   }
 });
 
