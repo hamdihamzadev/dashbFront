d70e9c24167ef71373a4785c21ef0691a7b92235
hamdi hamza <hamdihamzadev@hotmail.com>
2024-10-01 23:26:21 +0100

    add dist 01/10

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
-
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'? '/': '/',
-  devServer: {
-    historyApiFallback: true  
-  }
+  publicPath: process.env.NODE_ENV === 'production'? '/': '/'
 });
-
 
